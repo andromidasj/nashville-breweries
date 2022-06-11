9aa48a4df6e9f2b394c17eb1162363116f2e43ba
Josh Andromidas <66661368+andromidasj@users.noreply.github.com>
2022-06-10 22:56:21 -0500

    add detail page and integrate map API

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -3,53 +3,85 @@ import {
   Badge,
   Card,
   Center,
-  Container,
   Group,
   Image,
   Stack,
   Anchor,
+  Text,
+  Modal,
+  Title,
 } from "@mantine/core";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+
+const typeColors = {
+  micro: "blue",
+  nano: "yellow",
+  brewpub: "teal",
+  planning: "dark",
+  contract: "orange",
+};
+
+const cardStyle = {
+  maxWidth: 800,
+  margin: "auto",
+  marginTop: 10,
+  cursor: "pointer",
+};
+
+const cardHover = () => ({
+  "&:hover": {
+    transform: "scale(1.01)",
+  },
+});
 
 function Brewery(props) {
   const [details, setDetails] = useState({});
+  const navigate = useNavigate();
+  // const [opened, setOpened] = useState(false);
 
   useEffect(() => {
     axios
       .get(`https://api.openbrewerydb.org/breweries/${props.id}`)
       .then((res) => {
-        console.log(res.data);
+        // console.log(res.data);
         setDetails(res.data);
       });
   }, []);
 
+  const handleClick = (e) => {
+    if (e.target.target !== "_blank") {
+      navigate(`/${details.id}`);
+    }
+  };
+
+  const address = `${details.street || "TBA"}, ${details.city}, ${
+    details.state
+  } ${details.postal_code?.split("-")[0]}`;
+  const url = details.website_url?.split("http://")[1].split("www.").slice(-1);
+
   return (
-    <Container
-      style={{
-        backgroundColor: "white",
-        borderRadius: 15,
-        maxWidth: 600,
-        // height: 120,
-        marginTop: 10,
-      }}
-      py={15}
-      px={30}
+    <Card
+      style={cardStyle}
+      sx={cardHover}
+      radius="lg"
+      // component={Link}
+      // to={`/${details.id}`}
+      onClick={handleClick}
     >
       <Stack align="flex-start" spacing="sm">
         <Group position="apart" style={{ width: "100%" }}>
-          <h2>{details.name}</h2>
-          <Badge>{details.brewery_type}</Badge>
+          <Title order={2}>{details.name}</Title>
+          <Badge color={typeColors[details.brewery_type]}>
+            {details.brewery_type}
+          </Badge>
         </Group>
-        <span>
-          {details.street}, {details.city}, {details.state}{" "}
-          {details.postal_code?.split("-")[0]}
-        </span>
+        <Text>{address}</Text>
         <Anchor href={details.website_url} target="_blank" rel="noreferrer">
-          {details.website_url?.split("http://")[1]}
+          {url}
         </Anchor>
       </Stack>
-    </Container>
+    </Card>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import App from "./App";
+import BreweryDetails from "./components/BreweryDetails";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/:id" element={<BreweryDetails />} />
+    </Routes>
+  </BrowserRouter>
+);
